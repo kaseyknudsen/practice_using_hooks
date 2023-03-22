daf68e30a8f1403bed2b771c496975be86273a93
Kasey Knudsen <kaseyknudsen@gmail.com>
2023-03-21 23:06:42 -0700

    added an increment and decrement button

--- hooks-practice/src/App.js
+++ hooks-practice/src/App.js
@@ -1,11 +1,29 @@
-
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [score, setScore] = useState(0);
+  const [message, setMessage] = useState("Welcome!");
   return (
-    <div className="App">
-     
-    </div>
+    <>
+      <div className="App">
+        <h1>{message}</h1>
+        <h1>{score}</h1>
+        <button onClick={
+          () => {
+            setScore(score + 1)
+            setMessage("You Clicked Increment!")
+          }
+          }>click to increment</button>
+          <button style={{marginTop: '10px'}} onClick={
+          () => {
+            setScore(score - 1)
+            setMessage("You Clicked Decrement!")
+          }
+          }>click to decrement</button>
+      </div>
+      <div></div>
+    </>
   );
 }
 
